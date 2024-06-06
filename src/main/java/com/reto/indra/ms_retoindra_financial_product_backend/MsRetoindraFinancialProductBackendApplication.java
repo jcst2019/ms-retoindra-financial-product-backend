230de37ee36ce24c84a3c552005efb4fed3e69f4
jcst2019 <juansolar_4@hotmail.com>
2024-06-05 21:08:06 -0500

    commint Inicial

---
+++ src/main/java/com/reto/indra/ms_retoindra_financial_product_backend/MsRetoindraFinancialProductBackendApplication.java
@@ -0,0 +1,13 @@
+package com.reto.indra.ms_retoindra_financial_product_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsRetoindraFinancialProductBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsRetoindraFinancialProductBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/reto/indra/ms_retoindra_financial_product_backend/MsRetoindraFinancialProductBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.reto.indra.ms_retoindra_financial_product_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsRetoindraFinancialProductBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
