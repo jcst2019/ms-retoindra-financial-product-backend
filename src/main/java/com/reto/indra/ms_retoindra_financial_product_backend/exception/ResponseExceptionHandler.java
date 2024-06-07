2b7905bb67e1cf710facb945dab68e3c36aebfd2
jcst2019 <juansolar_4@hotmail.com>
2024-06-07 14:29:36 -0500

    Manejar Excepciones Globales V2

--- src/main/java/com/reto/indra/ms_retoindra_financial_product_backend/exception/ResponseExceptionHandler.java
+++ src/main/java/com/reto/indra/ms_retoindra_financial_product_backend/exception/ResponseExceptionHandler.java
@@ -1,6 +1,8 @@
 package com.reto.indra.ms_retoindra_financial_product_backend.exception;
 
 import com.fasterxml.jackson.databind.exc.InvalidTypeIdException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -14,19 +16,23 @@ import java.time.LocalDateTime;
 @ControllerAdvice
 public class ResponseExceptionHandler {
 
+	private static final Logger logger = LoggerFactory.getLogger(ResponseExceptionHandler.class);
 	// Este método maneja todas las excepciones no definidas específicamente
 	@ExceptionHandler(Exception.class)
 	@ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
 	public Mono<ResponseEntity<ExceptionResponse>> manejarTodasExcepciones(Exception ex, ServerWebExchange exchange) {
-		ExceptionResponse er = new ExceptionResponse(LocalDateTime.now(), ex.getMessage(),
+		String errorMessage = "Uncontrolled error, contact the systems area.";
+		ExceptionResponse er = new ExceptionResponse(LocalDateTime.now(), errorMessage,
 				exchange.getRequest().getURI().toString());
+		logger.error("Error Generico =>", ex);
 		return Mono.just(new ResponseEntity<>(er, HttpStatus.INTERNAL_SERVER_ERROR));
 	}
 
-	@ExceptionHandler(InvalidTypeIdException.class)
+	@ExceptionHandler({InvalidTypeIdException.class, IllegalStateException.class})
 	@ResponseStatus(HttpStatus.BAD_REQUEST)
-	public Mono<ResponseEntity<ExceptionResponse>> handleInvalidTypeIdException(InvalidTypeIdException ex, ServerWebExchange exchange) {
-		String errorMessage = "Invalid product type: " + ex.getTypeId() + " => " + ex.getMessage();
+	public Mono<ResponseEntity<ExceptionResponse>> handleInvalidTypeIdAndIllegalState(Exception ex, ServerWebExchange exchange) {
+		logger.error("Exception in createFinancialProduct", ex);
+		String errorMessage = "Exception: " + ex.getMessage();
 		ExceptionResponse er = new ExceptionResponse(LocalDateTime.now(), errorMessage,
 				exchange.getRequest().getURI().toString());
 		return Mono.just(new ResponseEntity<>(er, HttpStatus.BAD_REQUEST));
@@ -37,6 +43,7 @@ public class ResponseExceptionHandler {
 	public Mono<ResponseEntity<ExceptionResponse>> manejarModeloException(ModeloNotFoundException ex, ServerWebExchange exchange) {
 		ExceptionResponse er = new ExceptionResponse(LocalDateTime.now(), ex.getMessage(),
 				exchange.getRequest().getURI().toString());
+		logger.error("ModeloNotFoundException in createFinancialProduct", ex);
 		return Mono.just(new ResponseEntity<>(er, HttpStatus.NOT_FOUND));
 	}
 }
